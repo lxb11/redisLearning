50289d8b4ada1ff10a0970ee9d3896fa46e56547
lxb11 <623417725@qq.com>
2025-04-15 11:36:03 +0800

    商铺缓存添加超时剔除和主动更新

--- hm-dianping/src/main/java/com/hmdp/controller/ShopTypeController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/ShopTypeController.java
@@ -2,12 +2,14 @@ package com.hmdp.controller;
 
 
 import com.hmdp.dto.Result;
+import com.hmdp.entity.ShopType;
 import com.hmdp.service.IShopTypeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -27,7 +29,6 @@ public class ShopTypeController {
     public Result queryTypeList() {
 //        List<ShopType> typeList = typeService
 //                .query().orderByAsc("sort").list();
-        Result typeList = typeService.queryTypeList();
-        return Result.ok(typeList);
+        return typeService.queryTypeList();
     }
 }
--- hm-dianping/src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
@@ -15,10 +16,11 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -42,13 +44,14 @@ public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> i
             return Result.ok(shopTypes);
         }
         // 4.不存在，查询数据库
-        List<ShopType> typeList = this.query().orderByAsc("sort").list();
+        List<ShopType> typeList = query().orderByAsc("sort").list();
         // 5.不存在，返回空列表
         if (typeList.isEmpty()) {
             return Result.ok(Collections.emptyList());
         }
         // 6.存在，写入redis
-        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(typeList));
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(typeList),
+                RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
         return Result.ok(typeList);
     }
 }
